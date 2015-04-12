6903ba8191680d465da095cf78febaf9373816c5
江冲 <jackeyjiang123@gmall.com>
2015-04-12 23:24:50 +0800

    A VERSION FOR LWIP

---
+++ Project/Standalone/tftpserver/MDK-ARM/STM324xG_EVAL/diskio.d
@@ -0,0 +1,3 @@
+.\stm324xg_eval\diskio.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\src\diskio.c
+.\stm324xg_eval\diskio.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\inc\diskio.h
+.\stm324xg_eval\diskio.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\inc\integer.h
