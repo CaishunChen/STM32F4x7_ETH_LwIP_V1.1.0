5465017284c9f251289eda6ad21d2fcd63a0edb5
江冲 <jackeyjiang123@gmall.com>
2015-05-24 23:40:43 +0800

    none

--- Project/FreeRTOS/httpserver_netconn/MDK-ARM/STM324xG_EVAL/crc_16.d
+++
@@ -1,4 +0,0 @@
-.\stm324xg_eval\crc_16.o: ..\src\crc_16.c
-.\stm324xg_eval\crc_16.o: ..\inc\crc_16.h
-.\stm324xg_eval\crc_16.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\stm324xg_eval\crc_16.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
--- Project/Standalone/tftpserver/MDK-ARM/STM324xG_EVAL/ff.d
+++
@@ -1,5 +0,0 @@
-.\stm324xg_eval\ff.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\src\ff.c
-.\stm324xg_eval\ff.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\inc\ff.h
-.\stm324xg_eval\ff.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\inc\integer.h
-.\stm324xg_eval\ff.o: ..\inc\ffconf.h
-.\stm324xg_eval\ff.o: ..\..\..\..\Utilities\Third_Party\FatFs_vR0.09\inc\diskio.h
